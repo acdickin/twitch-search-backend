f8818ad595d207d131562d79c56b29b548caa112
Andrew Dickinson <andrew.dickinson@t2.local>
2018-03-31 15:17:07 -0700

    updated handlers

--- handlers.go
+++ handlers.go
@@ -4,66 +4,62 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"io/ioutil"
+
 	"github.com/gorilla/mux"
 )
 
-func VideoByIDHander(w http.ResponseWriter, re *http.Request) {
-	client := &http.Client{}
-	vars := mux.Vars(re)
-	id := vars["videoid"]
-	url := "https://api.twitch.tv/helix/videos?id=" + id
-	
-	fmt.Println("url: ",url)
-	req, err := http.NewRequest("GET", url, nil)
-	
-	req.Header.Add("Client-ID", "hq22yaqr8c24w8ymv4q0vhh9wrxzst")
-	fmt.Println("Header: ", req.Header)
-	if err != nil {
-		fmt.Println("Request error",err)
-		log.Fatal("NewRequest: ", err)
+func ErrorHandle(err, errorOn){
+	if err=! nil{
+		log.Fatal(errorOn, err)
 		return
 	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Do error",err)
-		log.Fatal("Do: ", err)
-		return
-	}
-	fmt.Println("Response: ",resp)
-	
-	respondWithJSON(w,http.StatusOK, resp)
-
-	defer resp.Body.Close()
-
-	var record Video
-	fmt.Println("Video id: ", record.id)
-	fmt.Println("Video Title: ", record.title)
-	fmt.Println("user id: ", record.user_id)
-
-
-	// if err != nil {
-	// 	respondWithError(w, http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
 }
 
-// func UserByIdFunc(w http.ResponseWriter, re *http.Request){
-// 	vars := mux.Vars(re)
-// 	id :=vars["userid"]
-//
-// 	if err != nil {
-// 		respondWithError(w, http.StatusInternalServerError, err.Error())
-// 		return
-// 	}
-// }
-//
-// func GameByIdFunc(w http.ResponseWriter, re *http.Request){
-// 	vars := mux.Vars(re)
-// 	id :=vars["gameid"]
-//
-// 	if err != nil {
-// 		respondWithError(w, http.StatusInternalServerError, err.Error())
-// 		return
-// 	}
-// }
+func DataRequest( idName,url ){
+	client := &http.Client{}
+	vars := mux.Vars(re)
+	id := vars[idName]
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Client-ID", "hq22yaqr8c24w8ymv4q0vhh9wrxzst")
+	req.Header.Add("Content-Type", "application/json")
+	ErrorHandle(err,"NewRequest")
+
+	resp, err := client.Do(req)
+	ErrorHandle(err,"Response: ")
+
+	defer resp.Body.Close()
+	
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	
+    bodyString := string(bodyBytes)
+    respondWithJSON(w,http.StatusOK, bodyString)
+}
+
+func VideoByIDHander(w http.ResponseWriter, re *http.Request) {
+	
+	
+	url := "https://api.twitch.tv/helix/videos?id=" + id
+	idName := "videoid"
+	DataRequest(idName, url)
+	
+	
+	
+
+func UserByIdFunc(w http.ResponseWriter, re *http.Request){
+	vars := mux.Vars(re)
+	id :=vars["userid"]
+
+	
+}
+
+func GameByIdFunc(w http.ResponseWriter, re *http.Request){
+	vars := mux.Vars(re)
+	id :=vars["gameid"]
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
